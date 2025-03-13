465d6cb4d61ee0c65e3732408e4e0e418f13f388
Lokendrajoci <lokendrajoci@gmail.com>
2025-03-13 22:21:59 +0545

    modified:   app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/api/v1/tours', (req, res) => {
   tours.push(newTour);
 
   try {
-    fs.writeFile(
+    fs.writeFileSync(
       `${__dirname}/dev-data/data/tours-simple.json`,
       JSON.stringify(tours)
     );
